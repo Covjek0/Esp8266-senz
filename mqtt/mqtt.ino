f81ff453d22925dd119eb11a862c3d6894f52947
Covjek0 <67098558+Covjek0@users.noreply.github.com>
2020-06-18 12:46:30 +0200

    basic mqtt conect
    basic mqtt conect

---
+++ mqtt/mqtt.ino
@@ -0,0 +1,69 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+ 
+
+
+const char* mqttServer = "m24.cloudmqtt.com";
+const int mqttPort = 10991;
+const char* mqttUser = "";
+const char* mqttPassword = "";
+const char* ssid = "";
+const char* password = "";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+ 
+void setup() {
+ 
+  Serial.begin(115200);
+ 
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting to WiFi..");
+  }
+  Serial.println("Connected to the WiFi network");
+ 
+  client.setServer(mqttServer, mqttPort);
+  client.setCallback(callback);
+ 
+  while (!client.connected()) {
+    Serial.println("Connecting to MQTT...");
+ 
+    if (client.connect("ESP8266Client2")) {
+ 
+      Serial.println("connected");  
+ 
+    } else {
+ 
+      Serial.print("failed with state ");
+      Serial.print(client.state());
+      delay(2000);
+ 
+    }
+  }
+ 
+  client.publish("test", "radi");
+  client.subscribe("test");
+ 
+}
+ 
+void callback(char* topic, byte* payload, unsigned int length) {
+ 
+  Serial.print("Message arrived in topic: ");
+  Serial.println(topic);
+ 
+  Serial.print("Message:");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+ 
+  Serial.println();
+  Serial.println("-----------------------");
+ 
+}
+ 
+void loop() {
+  client.loop();
+}
